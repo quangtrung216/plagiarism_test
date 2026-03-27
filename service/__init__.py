f02f5b74cfc507dea8905533a3c79519b8030ece
quangtrung216 <lequangtrungtrung345xx@gmail.com>
2026-03-27 15:31:15 +0700

    thay đổi docker compose

--- service/__init__.py
+++ service/__init__.py
@@ -4,6 +4,15 @@ Service module for document embedding and similarity detection.
 
 from .embedding_service import DEk21HCMUTEEmbedding, EmbeddingService
 from .document_processor import DocumentProcessor
+
+__all__ = ['DEk21HCMUTEEmbedding', 'EmbeddingService', 'DocumentProcessor']
+
+"""
+Service module for document embedding and similarity detection.
+"""
+
+from .embedding_service import DEk21HCMUTEEmbedding, EmbeddingService
+from .document_processor import DocumentProcessor
 from .highlight_service import HighlightService
 
 __all__ = ['DEk21HCMUTEEmbedding', 'EmbeddingService', 'DocumentProcessor', 'HighlightService']
